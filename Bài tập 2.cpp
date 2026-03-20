ca71df70445904803512f23d92be2caccc4608d6
tai210507 <tai210507@gmail.com>
2026-03-20 11:29:08 +0700

    Update Bài tập 2.cpp

--- Bài tập 2.cpp
+++ Bài tập 2.cpp
@@ -1,47 +1,65 @@
 #include <iostream>
 using namespace std;
 
+// Khai báo cấu trúc lưu trữ dữ liệu cho Phân Số
 struct PhanSo
 {
-    int tu, mau;
+    int tu;  
+    int mau; 
 };
 
+// Hàm nhập dữ liệu cho phân số
 void nhapPhanSo(PhanSo &ps)
 {
-    cin >> ps.tu;
+    cin >> ps.tu; 
+    
+    // Vòng lặp do-while để bắt buộc người dùng nhập mẫu số khác 0
     do
-        {
+    {
         cin >> ps.mau;
-        } while (ps.mau == 0);
+    } while (ps.mau == 0); 
 }
 
+// Hàm in phân số ra màn hình
 void xuatPhanSo(PhanSo ps)
 {
-    cout << ps.tu << "/" << ps.mau;
+    cout << ps.tu << "/" << ps.mau; // In theo định dạng Tử/Mẫu
 }
 
+// Hàm tính giá trị thực phân số ra số thập phân
 double giaTri(PhanSo ps)
 {
+    // Ép kiểu (double) cho tử số để kết quả phép chia là số thực (có phần thập phân)
     return (double)ps.tu / ps.mau;
 }
 
+// Hàm chính của chương trình
 int main()
 {
-    PhanSo ps1, ps2;
+    PhanSo ps1, ps2; 
+    
+    
     nhapPhanSo(ps1);
     nhapPhanSo(ps2);
+    
+    // So sánh giá trị thập phân của hai phân số
     if (giaTri(ps1) > giaTri(ps2))
-        {
+    {
+        // Nếu phân số 1 lớn hơn, in phân số 1
         xuatPhanSo(ps1);
-        } else if (giaTri(ps1) < giaTri(ps2))
-        {
+    } 
+    else if (giaTri(ps1) < giaTri(ps2))
+    {
+        // Nếu phân số 2 lớn hơn, in phân số 2
         xuatPhanSo(ps2);
-        } else
-        {
+    } 
+    else
+    {
+        // Nếu giá trị bằng nhau
         cout << "Hai phan so bang nhau";
-        }
-    cout << endl;
+    }
+    
+    cout << endl; 
 
-    return 0;
+    return 0; // Kết thúc chương trình
 }
-
